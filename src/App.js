bdb6d4be02b3b2df6aba3846b1389c8ca5904f2f
musayuksel <musayuxel@gmail.com>
2021-10-10 15:43:48 +0100

    feat:Add Level-1 Show scores, style:Add basic styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import EachScoreTable from "./EachScoreTable";
+import allCountryScores from "./scores";
 
 function App() {
-  return <div className="App"> Initial App</div>;
+  return (
+    <div className="App">
+      <section className="scoreContainer">
+        <h2>High Scores per Country</h2>
+        {allCountryScores.map((country, i) => (
+          <EachScoreTable key={i} country={country} />
+        ))}
+      </section>
+    </div>
+  );
 }
 
 export default App;
