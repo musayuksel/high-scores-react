53e06bcf01230e408aefc27995063c3501424823
musayuksel <musayuxel@gmail.com>
2021-10-10 15:52:45 +0100

    feat: Add Level-2  High Score Tables sorted alphabetically by country name

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import EachScoreTable from "./EachScoreTable";
 import allCountryScores from "./scores";
 
 function App() {
+  allCountryScores.sort((a, b) => {
+    return a.name < b.name ? -1 : 1;
+  });
   return (
     <div className="App">
       <section className="scoreContainer">
