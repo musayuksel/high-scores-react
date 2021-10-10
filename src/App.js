3e6a87dfd07708d3fd194e72e8bfaa3592be9710
musayuksel <musayuxel@gmail.com>
2021-10-10 18:34:18 +0100

    feat:Add level-3 level-4 Sort countries

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
+import { useState } from "react";
+import { useEffect } from "react/cjs/react.development";
 import "./App.css";
 import EachScoreTable from "./EachScoreTable";
 import allCountryScores from "./scores";
 
+let isAToZ = -1;
+let buttonText = "A-Z";
 function App() {
-  allCountryScores.sort((a, b) => {
-    return a.name < b.name ? -1 : 1;
-  });
+  const [orderList, setOrderList] = useState(allCountryScores);
+
+  function changeOrder() {
+    setOrderList(() =>
+      [...allCountryScores].sort((a, b) => {
+        return a.name.toUpperCase() < b.name.toUpperCase()
+          ? isAToZ
+          : -1 * isAToZ;
+      })
+    );
+    isAToZ *= -1;
+  }
+
+  useEffect(() => (buttonText = isAToZ === -1 ? "A-Z" : "Z-A"));
+
   return (
     <div className="App">
       <section className="scoreContainer">
+        <button className="orderList" onClick={changeOrder}>
+          Sort: {buttonText}
+        </button>
         <h2>High Scores per Country</h2>
-        {allCountryScores.map((country, i) => (
-          <EachScoreTable key={i} country={country} />
+        {orderList.map((country) => (
+          <EachScoreTable key={country.name} country={country} />
         ))}
       </section>
     </div>
